3a72ddec1dcadad2e3940fc5605a84bc39d1f4e2
SetItOnZygote <70650586+SetItOnZygote@users.noreply.github.com>
2021-02-22 19:51:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(config.MongoURL, {
 });
 
 client.on("ready", async () => {
-    console.log(`${client.user.tag} is Online and ready to Watch quick.db\'s Failure jkjk unless...`)
+    console.log(`${client.user.tag} is Online!`)
     client.user
    function randomStatus() {
     let status = ["discord.gg/tca", "Ping for Prefix!", `over ${client.users.cache.size} Online Friends! <3`, `over ${client.guilds.cache.size} Servers! <3`]
