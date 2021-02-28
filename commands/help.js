fd267766c8175bfaac3c1c27df164047b0063cdf
SetItOnZygote <70650586+SetItOnZygote@users.noreply.github.com>
2021-02-28 15:14:02 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "help",
+    guildOnly: false,
+    description: "show da commands",
+    cooldown: 4,
+    aliases: ["commands"],
+    clientPermissions: [],
+    userPermissions: [],
+    async execute(message, args, Discord, client) {
+            const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("Commands!")
+            .setDescription(`Command #1: \`encodebase64\`\nEncode text into base64!\n\Command #2: \`decodebase64\`\nDecode Base64 into text!\n\nCommand #3: \`encodebinary\`\nConvert Text into Binary Text!\n\nCommand #4: \`decodebinary\`\nDecode Binary Text into Text!\n\nCommand #5: \`setprefix\`\nChange Bot Prefix!`)
+            .setFooter(`Ping me for prefix!`) 
+            .setTimestamp();
+            message.channel.send(embed)
+    }
+}
