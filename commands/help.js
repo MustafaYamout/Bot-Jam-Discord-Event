92f28cc08ce2f2fc57c3958442e67c7a1a100f67
SetItOnZygote <70650586+SetItOnZygote@users.noreply.github.com>
2021-03-14 21:37:57 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
             .setColor("RANDOM")
             .setTitle("Commands!")
-            .setDescription(`Command #1: \`encodebase64\`\nEncode text into base64!\n\nCommand #2: \`decodebase64\`\nDecode Base64 into text!\n\nCommand #3: \`encodebinary\`\nConvert Text into Binary Text!\n\nCommand #4: \`decodebinary\`\nDecode Binary Text into Text!\n\nCommand #5: \`setprefix\`\nChange Bot Prefix!`)
+            .setDescription(`Command #1: \`encodebase64\`\nEncode text into base64!\n\nCommand #2: \`decodebase64\`\nDecode Base64 into text!\n\nCommand #3: \`encodebinary\`\nConvert Text into Binary Text!\n\nCommand #4: \`decodebinary\`\nDecode Binary Text into Text!\n\nCommand #5: \`setprefix\`\nChange Bot Prefix!\n\nCommand #6: \`encodemorse\`\nChange Text into morse code!\n\nCommand #7: \`decodemorse\`\Decode morse into text!`)
             .setTimestamp();
             message.channel.send(embed)
     }
